bf021ebfa81d6508874b3c93e15e4464b9abca73
rob seward <stuff@robs-MacBook-Pro.local>
2021-02-19 16:11:41 +0100

    firebase auth added

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDXUCNSpi5u07F76httlCTXAA4mPVQlEHs",
-  authDomain: "catch-of-the-day-wes-bos-2.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-wes-bos-2.firebaseio.com"
+  apiKey: "AIzaSyCXGTyBf67iu507Uza55JP6eTYHo8rlZgs",
+    authDomain: "catch-of-the-day-rob4130.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-rob4130-default-rtdb.europe-west1.firebasedatabase.app/",
 });
 
 const base = Rebase.createClass(firebaseApp.database());
